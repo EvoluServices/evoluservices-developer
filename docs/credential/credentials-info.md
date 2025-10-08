03f13c5e66b07637f5729a40281bd1ec44e3d597
douglas.rodrigues <douglas.rodrigues@evoluservices>
2025-10-08 18:07:24 -0300

    Criado componente de card e  credenciais.
     No componente de CARD com as especificações de dados de cartões para teste no Link de Pagamento e nas Credenciais inserimos as URL'S  e instaladores dividos por ambientes.

---
+++ docs/credential/credentials-info.md
@@ -0,0 +1,47 @@
+---
+sidebar_position: 1
+---
+# Credencias 
+
+## URL
+
+ A seguir estão listadas as URLs oficiais dos ambientes da EvoluServices, utilizadas para desenvolvimento e produção das integrações
+Obs: As informações de usuario e senha para ambos os ambientes foram enviados anteriormente por e-mail.
+
+| Ambiente       | URL                                   |
+|----------------|----------------------------------------|
+| Desenvolvimento | https://sandbox.evoluservices.com      |
+| Produção        | https://api.evoluservices.com          |
+
+
+
+## Instaladores
+
+ As tabelas a seguir contêm os links de instalação do programa necessário para o funcionamento do sistema da EvoluServices, separados por ambiente e por Segmento.
+ Obs: Se faz necessario o download de algum dos programas abaixo apenas para casos onde será feita a  integração da tecnologia do Pinpad (Maquina com Fio). Se irão integrar o POS (Maquina Sem fio) e/ou Order (Link de Pagamento), então esse passo não se faz necessário.
+
+
+
+### Ambiente de Produção
+| Segmento | Link de Instalação (Programa para Pinpad - Windows)                                                                                                                                                                                                                                                                   |
+|----------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| Saúde    | [Baixar Instalador Saude - Produção](https://evo-public-files.s3.sa-east-1.amazonaws.com/evcash-windows/production/AutoUpdate/SaudeService/Install/Setup.exe)  |
+| EVO      | [Baixar Instalador Evo - Produção](https://evo-public-files.s3.sa-east-1.amazonaws.com/evcash-windows/production/AutoUpdate/EvCash/Install/Setup.exe)          |
+| Finpet   | [Baixar Instalador Finpet - Produção](https://evo-public-files.s3.sa-east-1.amazonaws.com/evcash-windows/production/AutoUpdate/Finpet/Install/Setup.exe)       |
+
+
+### Ambiente de Sandbox
+| Segmento | Link de Instalação (Programa para Pinpad - Windows)                                                                                                       |
+|----------|-----------------------------------------------------------------------------------------------------------------------------------------------------------|
+| Saúde    | [Baixar Instalador Saude - Sandbox](https://evo-public-files.s3.sa-east-1.amazonaws.com/evcash-windows/sandbox/AutoUpdate/SaudeService/Install/Setup.exe) |
+| EVO      | [Baixar Instalador EVO - Sandbox](https://evo-public-files.s3.sa-east-1.amazonaws.com/evcash-windows/sandbox/AutoUpdate/EvCash/Install/Setup.exe)         |
+| Finpet   | [Baixar Instalador Finpet - Sandbox](https://evo-public-files.s3.sa-east-1.amazonaws.com/evcash-windows/sandbox/AutoUpdate/Finpet/Install/Setup.exe)      |
+
+
+
+
+
+
+
+
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,156 +6,162 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "EvoluServices",
-  tagline: "Meios de Pagamentos Ltda.",
-  url: "https://evoluservices.github.io",
-  baseUrl: "/evoluservices-developer/",
-  projectName: "evoluservices-developer",
-  organizationName: "EvoluServices",
-  trailingSlash: false,
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/evo.png",
+    title: "EvoluServices",
+    tagline: "Meios de Pagamentos Ltda.",
+    url: "https://evoluservices.github.io",
+    baseUrl: "/evoluservices-developer/",
+    projectName: "evoluservices-developer",
+    organizationName: "EvoluServices",
+    trailingSlash: false,
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/evo.png",
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      }),
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
+    themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: "EvoluServices",
-        logo: {
-          alt: "Evoluservices Logo",
-          src: "img/evo.png",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "intro",
-            label: "Introdução",
-            position: "left",
-          },
-          {
-            type: "doc",
-            docId: "payment-link/intro-payment-link",
-            label: "Link de Pagamento",
-            position: "left",
-          },
-          {
-            type: "doc",
-            docId: "remote-transaction/intro-remote-transaction",
-            label: "Transação Remota",
-            position: "left",
-          },
-          {
-            type: "doc",
-            docId: "contact",
-            label: "Contato",
-            position: "left",
-          },
-          {
-            href: "https://github.com/EvoluServices/evoluservices-developer",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Documentação",
-            items: [
-              {
-                label: "Introdução",
-                to: "/docs/intro",
-              },
-              {
-                label: "Link de Pagamento",
-                to: "/docs/payment-link/intro-payment-link",
-              },
-              {
-                label: "Transação Remota",
-                to: "/docs/remote-transaction/intro-remote-transaction",
-              },
-              {
-                label: "Contato",
-                to: "/docs/contact",
-              }
-            ],
-          },
-          {
-            title: "Serviços",
-            items: [
-              {
-                label: "Saúde Service",
-                href: "https://saudeservice.com.br/",
-              },
-              {
-                label: "Evo",
-                href: "https://www.sejaevo.com.br/",
-              },
-              {
-                label: "Finpet",
-                href: "https://www.finpet.com.br/",
-              },
-            ],
-          },
-          {
-            title: "Mais",
-            items: [
-              {
-                label: "Linkedin",
-                href: "https://br.linkedin.com/company/evoluservices",
-              },
-              {
-                label: "Instagram",
-                href: "https://www.instagram.com/evoluservices",
-              },
-              {
-                label: "Facebook",
-                href: "https://www.facebook.com/timeevoluservices",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} EvoluServices Meios de Pagamentos Ltda.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        ({
+            navbar: {
+                title: "EvoluServices",
+                logo: {
+                    alt: "Evoluservices Logo",
+                    src: "img/evo.png",
+                },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "intro",
+                        label: "Introdução",
+                        position: "left",
+                    },
+                    {
+                        type: "doc",
+                        docId: "payment-link/intro-payment-link",
+                        label: "Link de Pagamento",
+                        position: "left",
+                    },
+                    {
+                        type: "doc",
+                        docId: "remote-transaction/intro-remote-transaction",
+                        label: "Transação Remota",
+                        position: "left",
+                    },
+                    {
+                        type: "doc",
+                        docId: "credential/credentials-info",
+                        label: "Credenciais",
+                        position: "left",
+                    },
+                    {
+                        type: "doc",
+                        docId: "contact",
+                        label: "Contato",
+                        position: "left",
+                    },
+                    {
+                        href: "https://github.com/EvoluServices/evoluservices-developer",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Documentação",
+                        items: [
+                            {
+                                label: "Introdução",
+                                to: "/docs/intro",
+                            },
+                            {
+                                label: "Link de Pagamento",
+                                to: "/docs/payment-link/intro-payment-link",
+                            },
+                            {
+                                label: "Transação Remota",
+                                to: "/docs/remote-transaction/intro-remote-transaction",
+                            },
+                            {
+                                label: "Contato",
+                                to: "/docs/contact",
+                            }
+                        ],
+                    },
+                    {
+                        title: "Serviços",
+                        items: [
+                            {
+                                label: "Saúde Service",
+                                href: "https://saudeservice.com.br/",
+                            },
+                            {
+                                label: "Evo",
+                                href: "https://www.sejaevo.com.br/",
+                            },
+                            {
+                                label: "Finpet",
+                                href: "https://www.finpet.com.br/",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Mais",
+                        items: [
+                            {
+                                label: "Linkedin",
+                                href: "https://br.linkedin.com/company/evoluservices",
+                            },
+                            {
+                                label: "Instagram",
+                                href: "https://www.instagram.com/evoluservices",
+                            },
+                            {
+                                label: "Facebook",
+                                href: "https://www.facebook.com/timeevoluservices",
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} EvoluServices Meios de Pagamentos Ltda.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
 };
 
 module.exports = config;
